9cb985abd899b9d344bb656d27a15b299fb1598f
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-08-14 20:06:44 +0400

    scroll handlers are done

--- src/landing/sections/Second.jsx
+++ src/landing/sections/Second.jsx
@@ -82,7 +82,10 @@ const cards = [
 
 const Second = () => {
   return (
-    <section className="p-4 h-full bg-white text-primary_blue md:p-0 md:w-[80%] mx-auto md:mt-20 mt-14">
+    <section
+      id="services"
+      className="p-4 h-full bg-white text-primary_blue md:p-0 md:w-[80%] mx-auto md:mt-20 mt-14"
+    >
       <h2 className="font-extrabold  text-2xl mb-16 text-center md:text-start">
         Medical Services
       </h2>
